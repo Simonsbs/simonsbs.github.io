31b91c3c14c4c52091c10a640f73e95ab770a9bb
Simon B.Stirling <simonsbs@gmail.com>
2023-08-08 15:15:32 +0300

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Popper from "@popperjs/core";
+//import Popper from "@popperjs/core";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./App.scss";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Import HashRouter instead of BrowserRouter
 import Homepage from "./pages/Homepage";
 import Bloglist from "./pages/Bloglist";
 import Bloglist2 from "./pages/Bloglist2";
@@ -14,7 +14,7 @@ import BlogDetails2 from "./pages/BlogDetails2";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Homepage />} exact />
         <Route path="/blogs" element={<Bloglist />} exact />
@@ -22,7 +22,7 @@ function App() {
         <Route path="/blogs/:id/:title" element={<BlogDetails />} />
         <Route path="/blog-details/:id/:title" element={<BlogDetails2 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 

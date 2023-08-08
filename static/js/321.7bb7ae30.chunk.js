208ad35b619e7820ff674397bc17fb4b5292946d
Simon B.Stirling <simonsbs@gmail.com>
2023-08-08 13:44:36 +0300

    Updates

--- static/js/321.7bb7ae30.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkbako=self.webpackChunkbako||[]).push([[321],{8321:function(e,t,a){e.exports=a.p+"static/media/the-ugly-truth-about-design.a6c056dee77182ce5a0b.md"}}]);
---
+++ static/js/321.7bf269ca.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkbestdev=self.webpackChunkbestdev||[]).push([[321],{8321:function(e,t,s){e.exports=s.p+"static/media/the-ugly-truth-about-design.a6c056dee77182ce5a0b.md"}}]);
